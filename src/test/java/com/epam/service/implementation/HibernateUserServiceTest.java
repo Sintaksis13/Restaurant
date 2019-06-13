99fa5849d62f52e174b90f40f75d03b665e2b70e
Sintaksis13 <jandos130@mail.ru>
2019-06-13 18:31:48 +0700

    Fix NPE

--- src/test/java/com/epam/service/implementation/HibernateUserServiceTest.java
+++ src/test/java/com/epam/service/implementation/HibernateUserServiceTest.java
@@ -7,7 +7,9 @@ import com.epam.entity.type.Role;
 import com.epam.exception.DaoException;
 import javafx.util.Pair;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Collections;
 import java.util.List;
@@ -18,6 +20,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class HibernateUserServiceTest {
     @Mock
     private HibernateUserDao userDao;
